18cc028f12241550fe9d7b34ebb3d25b6a23a395
Moises159357 <116982321+Moises159357@users.noreply.github.com>
2026-09-12 02:53:37 -0500

    V1.1
    a

---
+++ src/main/java/org/example/trabajo_back/TrabajoBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.trabajo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/trabajo_back/TrabajoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trabajo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
