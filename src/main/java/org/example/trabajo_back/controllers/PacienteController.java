48b4553c8309328fa2b55c72fac828613ecf8b8d
Moises159357 <116982321+Moises159357@users.noreply.github.com>
2026-09-24 15:09:42 -0500

    V1.1.1.3
    Paciente

---
+++ src/main/java/org/example/trabajo_back/controllers/PacienteController.java
@@ -0,0 +1,94 @@
+package org.example.trabajo_back.controllers;
+
+import jakarta.validation.Valid;
+import org.example.trabajo_back.dtos.PacienteDTO;
+import org.example.trabajo_back.entities.Paciente;
+import org.example.trabajo_back.exceptions.ResourceNotFoundException;
+import org.example.trabajo_back.servicesinterfaces.IPacienteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/paciente")
+public class PacienteController {
+    private final IPacienteService pS;
+    private final ModelMapper modelMapper;
+
+    public PacienteController(IPacienteService pS, ModelMapper modelMapper) {
+        this.pS = pS;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/listarP")
+    public ResponseEntity<List<PacienteDTO>> listar(){
+        List<PacienteDTO> lista = pS.list()
+                .stream()
+                .map(paciente -> modelMapper.map(paciente, PacienteDTO.class))
+                .toList();
+
+        return ResponseEntity.ok(lista);
+    }
+
+    @PostMapping
+    @PreAuthorize("/registrarP/hasRole('ADMIN')")
+    public ResponseEntity<PacienteDTO> registrar(@Valid @RequestBody PacienteDTO dto ){
+        Paciente pc = modelMapper.map(dto, Paciente.class);
+
+        pS.insert(pc);
+
+        PacienteDTO responseDTO = modelMapper.map(pc, PacienteDTO.class);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(pc.getIdPaciente())
+                .toUri();
+        return ResponseEntity
+                .created(location)
+                .body(responseDTO);
+    }
+
+    @GetMapping("/buscarIDP/{id}")
+    public ResponseEntity<PacienteDTO> buscarId(@PathVariable Long id){
+        Paciente p = pS.listId(id).orElseThrow(() ->
+                new ResourceNotFoundException(
+                        "No existe el streaming con el id: " + id
+                ));
+
+        PacienteDTO dto = modelMapper.map(p, PacienteDTO.class);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping("/actualizarPaciente")
+    public ResponseEntity<PacienteDTO> actualizar(@Valid @RequestBody PacienteDTO dto){
+        Paciente existe = pS.listId(dto.getIdPaciente())
+                .orElseThrow(()->
+                        new ResourceNotFoundException(
+                                "No existe un paciente con el id: " + dto.getIdPaciente()
+                        )
+                );
+        Paciente paciente = modelMapper.map(dto, Paciente.class);
+        paciente.setIdPaciente(existe.getIdPaciente());
+        pS.update(paciente);
+        PacienteDTO responseDTO = modelMapper.map(paciente, PacienteDTO.class);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @DeleteMapping("/eliminarPaciente/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        Paciente paciente = pS.listId(id)
+                .orElseThrow(()->
+                        new ResourceNotFoundException(
+                                "No existe un paciente con el id: " + id
+                        )
+                );
+        pS.delete(paciente.getIdPaciente());
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/org/example/trabajo_back/entities/Citas.java
+++ src/main/java/org/example/trabajo_back/entities/Citas.java
@@ -1,4 +1,74 @@
 package org.example.trabajo_back.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "citas")
 public class Citas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCitas;
+    @Column(name = "fechaCita", nullable = false)
+    private LocalDate fechaCita;
+    @Column(name = "estado", length = 20, nullable = false)
+    private String estado;
+    @Column(name = "motivoCita", length =80, nullable = false)
+    private String motivoCita;
+
+    @ManyToOne
+    @JoinColumn(name = "idPaciente", nullable = false)
+    private Paciente paciente;
+
+    public Citas() {
+    }
+
+    public Citas(Long idCitas, LocalDate fechaCita, String estado, String motivoCita, Paciente paciente) {
+        this.idCitas = idCitas;
+        this.fechaCita = fechaCita;
+        this.estado = estado;
+        this.motivoCita = motivoCita;
+        this.paciente = paciente;
+    }
+
+    public Long getIdCitas() {
+        return idCitas;
+    }
+
+    public void setIdCitas(Long idCitas) {
+        this.idCitas = idCitas;
+    }
+
+    public LocalDate getFechaCita() {
+        return fechaCita;
+    }
+
+    public void setFechaCita(LocalDate fechaCita) {
+        this.fechaCita = fechaCita;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMotivoCita() {
+        return motivoCita;
+    }
+
+    public void setMotivoCita(String motivoCita) {
+        this.motivoCita = motivoCita;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
 }
